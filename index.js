0490726aa070ee2f76f8b12abe61d6e46fce7169
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-06-30 18:32:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var animal = 'dog'
-var yourAnimal = 'cat'
-
 function myAnimal() {
+  var animal = 'dog'
   return animal;
 }
 
@@ -9,6 +7,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  var yourAnimal = 'cat'
   return yourAnimal;
 }
 
