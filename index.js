0247ce4b8cae9a9dee646c734cdf4ca8e558ed21
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-06-30 18:23:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,11 +19,9 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
     return "FUNKY!"
-  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-console.log(funkyFunction());
+var theFunk = funkyFunction()
